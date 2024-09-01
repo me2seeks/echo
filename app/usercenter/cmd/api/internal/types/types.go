a49903a61cb318f515855af3147e6dc8dc2fe7c1
me2seeks <z3653934@163.com>
2024-09-01 15:30:54 +0800

    refactor: fix capitalization of handle field in user API

--- app/usercenter/cmd/api/internal/types/types.go
+++ app/usercenter/cmd/api/internal/types/types.go
@@ -69,7 +69,7 @@ type User struct {
 	Id       int64  `json:"id"`
 	Email    string `json:"email"`
 	Nickname string `json:"nickname"`
-	Handle   string `json:"Handle"`
+	Handle   string `json:"handle"`
 	Sex      int32  `json:"sex"`
 	Avatar   string `json:"avatar"`
 	Bio      string `json:"bio"`
